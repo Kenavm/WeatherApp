0abc6ec88963a1686faeddb474be72e66dbf1810
Manuel <manuel.vanek@gmail.com>
2023-01-17 22:23:08 +0100

    added suggestionhelper

--- backend.js
+++ backend.js
@@ -1,11 +1,11 @@
-async function getCityName (name) {
+async function suggestionsHelper (name) {
   const url = `http://api.weatherapi.com/v1/search.json?key=3063085c339a4700af7192624231701&q=${name}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  return data;
   }
   
-  getCityName("lon");
+  suggestionsHelper("lon");
 
   async function getWeatherData (name) {
   const url = `https://api.weatherapi.com/v1/forecast.json?key=3063085c339a4700af7192624231701&q=${name}`;
--- frontend.js
+++ frontend.js
@@ -2,10 +2,21 @@ const labelComponent = (text) => `<label>${text}</label>`
 const inputComponent = () => `<input id="input"></input>`
 const cardComponent = (id) => `<div id=${id}></div>`
 const heading = (city) => `<h2>${city}</h2>`
-const unorderedList = (temperature, skyConditions, humidty) => `<li>${temperature} </li>`
+const unorderedList = (temperature, skyConditions, humidty) => `<li>${temperature} </li> <li>${skyConditions} </li> <li>${humidty} </li>`
 
 const root = document.getElementById("root")
 
 root.innerHTML = labelComponent("Please put in a city name: ");
 root.innerHTML += inputComponent();
+
+const input = document.getElementById("input");
+
+input.addEventListener("input", async (e) => {
+    if(e.target.value.length >= 3) {
+        const suggestionList =  await suggestionsHelper(e.target.value);
+       for (const test of suggestionList) {
+        root.innerHTML += test.name; //todo: convert to suggestions in browser
+       }
+    }
+})
 
